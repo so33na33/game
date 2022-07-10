66f7ad5d003cd5b58272f0c041e0259738864a27
so33na33 <santhoshsona150@gmail.com>
2022-07-10 20:21:06 +0530

    game details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddGameComponent } from './add-game/add-game.component';
+import { ViewGameComponent } from './view-game/view-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddGameComponent
+    AddGameComponent,
+    ViewGameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-game/view-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-game',
+  templateUrl: './view-game.component.html',
+  styleUrls: ['./view-game.component.css']
+})
+export class ViewGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
